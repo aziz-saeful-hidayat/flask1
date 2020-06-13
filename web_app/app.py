538490964a7c80a8618fc05328e1a31d4b9feca3
Muhamad Fahrur Rasyid <mfahrurashyid@gmail.com>
2020-06-13 19:27:23 +0700

    Adding Bootstrap and Jinja

--- web_app/app.py
+++ web_app/app.py
@@ -1,13 +1,20 @@
-from flask import  Flask
+from flask import Flask, render_template
+
+
 def create_app():
     app = Flask(__name__)
+    app.config.from_pyfile('settings.py')
 
     @app.route('/')
     def index():
-        return 'Hello Flask'
+        return render_template('index.html', TITLE='Flask-01')
 
     @app.route('/blog')
     def blog():
         return 'Ini blog'
+
+    @app.route('/about')
+    def about():
+        return render_template('about.html', TITLE='About me')
 
     return app
